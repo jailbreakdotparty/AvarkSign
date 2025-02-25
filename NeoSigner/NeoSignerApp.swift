f1735d47a7f3ee7026d6d073dd35287cbec76ec9
Skadz <skadz@skadz.online>
2025-02-24 21:10:56 -0700

    Initial Commit

---
+++ NeoSigner/NeoSignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoSignerApp.swift
+//  NeoSigner
+//
+//  Created by Skadz on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NeoSignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
